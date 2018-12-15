15653037a2b93d5570b711e873d058a05f145197
Orlando Watson <ogw@orlandogwatson.com>
2018-12-15 10:32:09 -0800

    render asset classes in loop

--- wp-content/themes/equius-official/archive-asset_classes.php
+++ wp-content/themes/equius-official/archive-asset_classes.php
@@ -11,9 +11,29 @@
 ?>
   <main class="archive__asset_classes">
     <?php if ( have_posts() ) { ?>
-
       <?php get_template_part( 'template-parts/content', 'most-recent-asset-class' ) ?>
+      <?php get_template_part( 'template-parts/widgets/mailing', 'list' ); ?>
 
+      <div class="asset_classes__content">
+        <header>
+          <h2>Asset Class Archives</h2>
+        </header>
+        <section>
+          <?php while( have_posts() ) : $asset_class = the_post(); ?>
+  
+          <div class="asset_classes__class">
+            <h5><?php echo date("F Y", time($asset_class["post_date"])) ?></h5>
+            <h3><?php echo get_the_title($asset_class["ID"]) ?></h3>
+            <?php echo the_content(); ?>
+            <a href="<?php echo get_field( 'asset_class_pdf' ); ?>" download title="Download Asset Class">
+              <i class="fas fa-arrow-down"></i>
+              <span>download pdf</span>
+            </a>
+          </div>
+  
+          <?php endwhile; ?>
+        </section>
+      </div>
     <?php } else { ?>
 
       <section class="asset_classes__empty">
