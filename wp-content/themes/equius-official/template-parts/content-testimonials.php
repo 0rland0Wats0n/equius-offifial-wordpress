c3c373b2694ae135badd84ff05183cbcfe30cd7f
Orlando Watson <ogw@orlandogwatson.com>
2018-12-18 16:06:54 -0800

    handle testimonials empty state

--- wp-content/themes/equius-official/template-parts/content-testimonials.php
+++ wp-content/themes/equius-official/template-parts/content-testimonials.php
@@ -6,7 +6,7 @@
    */
 
   /**
-   * Get team members
+   * Get testimonials
    */
   $args = array(
     'meta_key'          => 'testimonial_attestant',
@@ -22,22 +22,36 @@
 
     <section class="widget__testimonials">
       <div class="testimonials__content">
-        <span class="object__arrow_left">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/arrow_left.png'?>" alt="">
-        </span>
+        <?php if ( $the_query->post_count > 1 ) : ?>
+          <span class="object__arrow_left">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/arrow_left.png'?>" alt="">
+          </span>
+        <?php endif; ?>
         <ul class="testimonials__items">
-          <?php while( $the_query->have_posts() ): $the_query->the_post(); ?>
-            <li>
+          <?php $i = 1; while( $the_query->have_posts() ): $the_query->the_post(); ?>
+            <li data-testimonial="<?php echo $i ?>"
+              data-state="<?php echo $i == 1 ? 'active' :'inactive' ?>">
               <blockquote><?php echo get_field( 'testimonial_details' ); ?></blockquote>
               <p><?php echo get_field( 'testimonial_attestant' ) ?></p>
               <p><?php echo get_field( 'testimonial_job_title' ) . ', ' . get_field( 'testimonial_company' ) ?></p>
             </li>
           <?php endwhile; ?>
         </ul>
-        <span class="object__arrow_right">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/arrow_right.png'?>" alt="">
-        </span>
+        <?php if ( $the_query->post_count > 1 ) : ?>
+          <span class="object__arrow_right">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/arrow_right.png'?>" alt="">
+          </span>
+        <?php endif; ?>
       </div>
+      
+      <?php if ( $the_query->post_count > 1 ) : ?>
+      <ul class="testimonials__switchers">
+        <?php $i = 1; while( $the_query->have_posts() ): $the_query->the_post(); ?>
+          <li data-testimonial="<?php echo $i ?>"
+            data-state="<?php echo $i == 1 ? 'active' :'inactive' ?>"></li>
+        <?php $i++; endwhile; ?>
+      </ul>
+      <?php endif; ?>
     </section>
 
 <?php 
