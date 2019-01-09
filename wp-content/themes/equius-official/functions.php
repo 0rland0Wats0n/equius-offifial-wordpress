b17e05e2c817c0b318d77e8c2ba003e1d06b9b36
Orlando Watson <ogw@orlandogwatson.com>
2019-01-08 19:02:50 -0800

    fix category search post cards layout

--- wp-content/themes/equius-official/functions.php
+++ wp-content/themes/equius-official/functions.php
@@ -139,6 +139,11 @@ if ( ! function_exists( 'equius_official_setup' ) ) :
 		}
 		add_filter('excerpt_more', 'new_excerpt_more');
 
+		function cc_mime_types($mimes) {
+			$mimes['svg'] = 'image/svg+xml';
+			return $mimes;
+		}
+		add_filter('upload_mimes', 'cc_mime_types');
 
 		/**
 		 * Register new widget areas
